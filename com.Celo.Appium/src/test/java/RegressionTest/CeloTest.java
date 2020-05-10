33f90804201c15cfad7bee75dcddb70f29f333a9
Manish Kumar(OSV) <manish.kumar.osv@fedex.com>
2020-05-10 19:13:34 +0530

    Camera feature Test Script

--- com.Celo.Appium/src/test/java/RegressionTest/CeloTest.java
+++ com.Celo.Appium/src/test/java/RegressionTest/CeloTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import pageObjects.Camera;
 import pageObjects.Library;
 import pageObjects.MoreModule;
 import utility.CaptureScreenshot;
@@ -23,8 +24,7 @@ public class CeloTest extends CeloBase {
 	//driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
 	
 	//@Test
-	public void run() throws MalformedURLException, InterruptedException{
-		
+	public void run() throws MalformedURLException, InterruptedException{	
 		//AndroidDriver<AndroidElement> driver = capabilities();
 		
 		MoreModule mm = new MoreModule(driver);
@@ -37,7 +37,7 @@ public class CeloTest extends CeloBase {
 		//mm.logOutButton.click();		
 	}
 	
-	@Test
+	//@Test
 	public void libratyModule() throws MalformedURLException, InterruptedException{
 		
 		Library lb= new Library(driver);
@@ -45,23 +45,32 @@ public class CeloTest extends CeloBase {
 		
 		Thread.sleep(3000);		
 		lb.libraryModuleBtn.click();
-		Thread.sleep(5000);
-		
-		lb.imageClick.get(0).click();
-		
+		Thread.sleep(5000);		
+		lb.imageClick.get(0).click();		
 		lb.shareBtn.click();
-		
 		lb.clickOUserName.click();
-
 		lb.sendPhotoBtn.click();
 		Thread.sleep(5000);
 		lb.timeView.click();
-		
-		//driver.navigate().back();
-		//driver.navigate().back();
 		Thread.sleep(2000);
-		
 		cs.takeScreenshot();
+		driver.navigate().back();
+		driver.navigate().back();
 }	
-	
+	//@Test
+	public void capturePhotoUsingCamera() throws InterruptedException{
+		
+		Camera ca= new Camera(driver);
+		
+		ca.cameraModuleBtn.click();
+		ca.cameraAllowPopup.click();
+		ca.captureBtn.click();
+		ca.cameraSaveBtn.click();
+		Thread.sleep(10000);
+		ca.imageClick.get(0).click();
+		ca.cameraShareBtn.click();
+		ca.selectUser.click();
+		ca.sendPhotoBtn.click();
+		
+	}
 }
